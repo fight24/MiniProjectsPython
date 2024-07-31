8d71e18db2c8322935e7f8c06f9ef25dd02d5fac
fight24 <swordartonline2421@gmail.com>
2024-07-31 22:43:14 +0700

    feat: initial project

--- Day31-Flash_Card_App_Capstone_Project/main.py
+++ Day31-Flash_Card_App_Capstone_Project/main.py
@@ -1,36 +1,59 @@
 from tkinter import *
 import pandas
 import random
-import time
-dataFrame = pandas.read_csv(r"E:\down\python_100days\practice_py_100days\100DaysPython\Day31"
-                            r"-Flash_Card_App_Capstone_Project\data\french_words.csv")
-to_learn = dataFrame.to_dict(orient="records")
+to_learn = {}
+try:
+    dataFrame = pandas.read_csv("./data/words_to_learn.csv")
+except FileNotFoundError as e:
+    dataFrame = pandas.read_csv("./data/french_words.csv")
+    to_learn = dataFrame.to_dict(orient="records")
+else:
+    to_learn = dataFrame.to_dict(orient="records")
 BACKGROUND_COLOR = "#B1DDC6"
 FONT_BOLD = ("Ariel", 60, "bold")
 FONT_ITALIC = ("Ariel", 40, "italic")
-c = 0
+current_card = {}
 
 
 # ---------------------------- Pick a random French word/translation ------------------------------- #
 def next_card():
-    global c
-    print(random.choice(to_learn))
-    random_data = random.choice(to_learn)
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    print(current_card)
     title_name = "French"
-    random_word = random_data[title_name]
-    canvas.itemconfig(title, text=f"{title_name}")
-    canvas.itemconfig(word, text=f"{random_word}")
-    if c > 0:
-        time.sleep(3)
-        back_card(random_data)
-    c += 1
+    random_word = current_card[title_name]
+    canvas.itemconfig(img_card, image=card_font)
+    canvas.itemconfig(title, text=f"{title_name}", fill="black")
+    canvas.itemconfig(word, text=f"{random_word}", fill="black")
+    flip_timer = window.after(3000, flip_card)
+
+#
+#
+# def eng_card(r):
+#     canvas.itemconfig(img_card, image=card_back)
+#     canvas.itemconfig(title, fill="white", text="English")
+#     canvas.itemconfig(word, fill="white", text=f"{r['English']}")
+#     print("Back card")
 
 
-def back_card(r):
+def flip_card():
     canvas.itemconfig(img_card, image=card_back)
     canvas.itemconfig(title, fill="white", text="English")
-    canvas.itemconfig(word, fill="white", text=f"{r['English']}")
-    print("Back card")
+    canvas.itemconfig(word, fill="white", text=f"{current_card['English']}")
+    print("English card")
+
+
+def is_known():
+    to_learn.remove(current_card)
+    print(len(to_learn))
+    save_file(to_learn)
+    next_card()
+
+
+def save_file(works_to_learn):
+    words_to_learn_df = pandas.DataFrame(works_to_learn)
+    words_to_learn_df.to_csv("./data/words_to_learn.csv", index=False)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -48,12 +71,14 @@ img_card = canvas.create_image(400, 263, image=card_font)
 title = canvas.create_text(400, 150, text="Title", font=FONT_ITALIC)
 word = canvas.create_text(400, 263, text="Word", font=FONT_BOLD)
 canvas.grid(column=0, row=0, columnspan=2)
-next_card()
+# data_list = next_card()
+# window.after(3000, eng_card, data_list)
 btn_w = Button(image=img_button_w, highlightthickness=0, bg=BACKGROUND_COLOR, bd=0, relief="flat", command=next_card)
 btn_w.grid(column=0, row=1)
 
-btn_r = Button(image=img_button_r, highlightthickness=0, bg=BACKGROUND_COLOR, bd=0, relief="flat", command=next_card)
+btn_r = Button(image=img_button_r, highlightthickness=0, bg=BACKGROUND_COLOR, bd=0, relief="flat", command=is_known)
 btn_r.grid(column=1, row=1)
-
+flip_timer = window.after(3000, flip_card)
+next_card()
 window.mainloop()
 
