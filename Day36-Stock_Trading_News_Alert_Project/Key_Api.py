e4b7347db9c6952287a4ddf67bb3465cf0c3eef0
fight24 <swordartonline2421@gmail.com>
2024-10-20 18:11:15 +0700

    fix: :bug: fix key

---
+++ Day36-Stock_Trading_News_Alert_Project/Key_Api.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class Key_Api(Enum):
+    KEY_NEW= 'your_key'
+    KEY_STOCK = 'your_key'
+    KEY_RAPIDAPI = "your_key"
+    KEY_ALPHA_VANTAGE = "your_key"
+    KEY_TWILIO = 'your_key'
+    ACC_ID = "your_id"
+    NUM_PHONE = 'u_phone'
+    MY_NUM = 'u_phone'
